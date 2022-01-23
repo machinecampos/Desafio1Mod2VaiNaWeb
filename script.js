241c975def2737d2a4eea3eea7f1cd53e3458166
Eduardo Campos <edumachadocampos@gmail.com>
2022-01-23 17:03:37 -0300

    Atribuição de valores a variáveis e operações matemáticas

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = "Eduardo"
+let num = 300
+let anoMenosNum = 2022 - num
+console.log(anoMenosNum + num)
+console.log("Estou mexendo no console.log")
+let quartaFeira = false
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
